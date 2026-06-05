1d32b8202e9b7e9bc694af164044f3b52b3d551e
ReturnKartikey <kartikeynegi2000.work@gmail.com>
2026-06-05 13:44:12 +0530

    Enable DevTools in development mode

--- electron/main.js
+++ electron/main.js
@@ -117,6 +117,7 @@ function createWindow() {
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
   }
